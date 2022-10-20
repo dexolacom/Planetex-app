05775dcf79cbf315e014743a227d085fe677623b
happy-cutman <happy.cutman@gmail.com>
2022-10-20 15:01:33 +0300

    ADD | wallets icons

--- src/components/ConnectWalletModal/ConnectWalletModal.tsx
+++ src/components/ConnectWalletModal/ConnectWalletModal.tsx
@@ -1,28 +1,39 @@
 import React from 'react';
-import { Wrapper, Content, WalletButton, CrossIcon } from './styles';
+import { Wrapper, Content, WalletButton, CrossIcon, MetamaskIcon, CoinbaseIcon, WalletConnectIcon } from './styles';
 import { connectors } from '../../constants/connectors';
 import { connectWallet } from '../../utils/blockchainUtils';
 import { useWeb3React } from '@web3-react/core';
+// @ts-ignore
+import coinbase from '../../assets/images/coinbase.png';
 
 
-const ConnectWalletModal = ({setIsModalOpen}: {setIsModalOpen: (b: boolean) => string}) => {
-  const { activate } = useWeb3React()
+const ConnectWalletModal = ({ setIsModalOpen }: { setIsModalOpen: (b: boolean) => string }) => {
+  const { activate } = useWeb3React();
 
   const connectWalletHandler = (connector: string) => {
-    connectWallet(activate, connectors[connector as keyof typeof connectors], connector)
-    setIsModalOpen(false)
+    connectWallet(activate, connectors[connector as keyof typeof connectors], connector);
+    setIsModalOpen(false);
   };
 
   return (
     <Wrapper onClick={(e) => e.stopPropagation()}>
-      <CrossIcon onClick={() => setIsModalOpen(false)}/>
+      <CrossIcon onClick={() => setIsModalOpen(false)} />
       <Content>
-        <WalletButton onClick={() => connectWalletHandler('injected')}>Metamask</WalletButton>
-        <WalletButton onClick={() => connectWalletHandler('walletConnect')}>WalletConnect</WalletButton>
-        <WalletButton onClick={() => connectWalletHandler('coinbaseWallet')}>CoinBase</WalletButton>
+        <WalletButton onClick={() => connectWalletHandler('injected')}>
+          <MetamaskIcon />
+          Metamask
+        </WalletButton>
+        <WalletButton onClick={() => connectWalletHandler('coinbaseWallet')}>
+          <CoinbaseIcon />
+          CoinBase
+        </WalletButton>
+        <WalletButton onClick={() => connectWalletHandler('walletConnect')}>
+          <WalletConnectIcon />
+          WalletConnect
+        </WalletButton>
       </Content>
     </Wrapper>
   );
 };
 
-export default ConnectWalletModal;
+  export default ConnectWalletModal;
