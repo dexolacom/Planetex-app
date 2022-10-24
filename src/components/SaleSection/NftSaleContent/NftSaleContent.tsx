1d78598de305c70e4f3141787a042c6e262f0a6e
Maxim280596 <max280596@gmail.com>
2022-10-24 20:38:16 +0300

    ADD| Buy

--- src/components/SaleSection/NftSaleContent/NftSaleContent.tsx
+++ src/components/SaleSection/NftSaleContent/NftSaleContent.tsx
@@ -31,88 +31,126 @@ import {
 } from './styles';
 import { SolidButton } from '../../../theme';
 import { useWeb3React } from '@web3-react/core';
+import Loader from '../../Loader/Loader';
+import StatusModal from '../../StatusModal/StatusModal';
+import ModalBackdrop from '../../ModalBackdrop/ModalBackdrop';
+import { checkApproveNft } from '../../../utils/blockchainUtils';
 
 const NftSaleContent = () => {
-  const { chainId } = useWeb3React();
+  const { chainId, account } = useWeb3React();
   const [tokenName, setTokenName] = useState('');
+  const [isTransErrorModal, setIsTransErrorModal] = useState(false);
+  const [isTransSuccessModal, setIsTransSuccessModal] = useState(false);
+  const [isTransLoading, setIsTransLoading] = useState(false);
+  // const;
 
   return (
-    <FullScreenTheme>
-      <Wrapper>
-        <Content>
-          <HeroGroupImg src={NFTSaleHeroGroup} />
-          <PriceContainer>
-            <PriceText>49$</PriceText>
-          </PriceContainer>
-          <MainContentWrap>
-            <TextContainer>
-              <Title>NFT-Sale</Title>
-              <Text>
-                Stake your SEAN up to 35 days to earn extra SEAN.ur SEAN up to
-                35 days to earn extra SEays to earn extra SEto earn extra
-                SEAN.ur SEAN up to 35 days extra SEAN.ur SEAN up to 35 days to
-                earn extra SEays to earn extra SEto earn extra SEAN.ur SEA
-              </Text>
-            </TextContainer>
-            <SkillsContainer>
-              <SkillTitle>Сharacter skills</SkillTitle>
-              <Skills>
-                <Skill>
-                  <SkillContent>
-                    <SkillImg src={NFTSaleDamage} />
-                    <SkillText>Damage</SkillText>
-                  </SkillContent>
-                </Skill>
-                <Skill>
-                  <SkillContent>
-                    <SkillImg src={NFTSaleSpeed} />
-                    <SkillText>Speed</SkillText>
-                  </SkillContent>
-                </Skill>
-                <Skill>
-                  <SkillContent>
-                    <SkillImg src={NFTSaleHealth} />
-                    <SkillText>Health</SkillText>
-                  </SkillContent>
-                </Skill>
-                <Skill>
-                  <SkillContent>
-                    <SkillImg src={NFTSaleArmor} />
-                    <SkillText>Armor</SkillText>
-                  </SkillContent>
-                </Skill>
-              </Skills>
-            </SkillsContainer>
-          </MainContentWrap>
-          <ActionContainer>
-            <Action>
-              <Select
-                id="tokenSelect"
-                value={tokenName}
-                onChange={(e) => setTokenName(e.target.value)}
-              >
-                {chainId === 97 || chainId === 56 ? (
-                  <>
-                    <option value="BNB">BNB</option>
-                    <option value="BUSD">BUSD</option>
-                  </>
-                ) : (
+    <>
+      <FullScreenTheme>
+        <Wrapper>
+          <Content>
+            <HeroGroupImg src={NFTSaleHeroGroup} />
+            <PriceContainer>
+              <PriceText>49$</PriceText>
+            </PriceContainer>
+            <MainContentWrap>
+              <TextContainer>
+                <Title>NFT-Sale</Title>
+                <Text>
+                  Stake your SEAN up to 35 days to earn extra SEAN.ur SEAN up to
+                  35 days to earn extra SEays to earn extra SEto earn extra
+                  SEAN.ur SEAN up to 35 days extra SEAN.ur SEAN up to 35 days to
+                  earn extra SEays to earn extra SEto earn extra SEAN.ur SEA
+                </Text>
+              </TextContainer>
+              <SkillsContainer>
+                <SkillTitle>Сharacter skills</SkillTitle>
+                <Skills>
+                  <Skill>
+                    <SkillContent>
+                      <SkillImg src={NFTSaleDamage} />
+                      <SkillText>Damage</SkillText>
+                    </SkillContent>
+                  </Skill>
+                  <Skill>
+                    <SkillContent>
+                      <SkillImg src={NFTSaleSpeed} />
+                      <SkillText>Speed</SkillText>
+                    </SkillContent>
+                  </Skill>
+                  <Skill>
+                    <SkillContent>
+                      <SkillImg src={NFTSaleHealth} />
+                      <SkillText>Health</SkillText>
+                    </SkillContent>
+                  </Skill>
+                  <Skill>
+                    <SkillContent>
+                      <SkillImg src={NFTSaleArmor} />
+                      <SkillText>Armor</SkillText>
+                    </SkillContent>
+                  </Skill>
+                </Skills>
+              </SkillsContainer>
+            </MainContentWrap>
+            <ActionContainer>
+              <Action>
+                <Select
+                  id="tokenSelect"
+                  value={tokenName}
+                  onChange={(e) => setTokenName(e.target.value)}
+                >
                   <>
                     <option value="ETH">ETH</option>
                     <option value="USDT">USDT</option>
                   </>
-                )}
-              </Select>
-            </Action>
-            <Action>
-              <SolidButton onClick={() => console.log('=> Mint')}>
-                Mint
-              </SolidButton>
-            </Action>
-          </ActionContainer>
-        </Content>
-      </Wrapper>
-    </FullScreenTheme>
+                </Select>
+              </Action>
+              <Action>
+                <SolidButton
+                  disabled={isTransLoading}
+                  onClick={() => {
+                    checkApproveNft(
+                      chainId,
+                      account,
+                      tokenName,
+                      setIsTransSuccessModal,
+                      setIsTransErrorModal,
+                      setIsTransLoading,
+                    );
+                  }}
+                >
+                  {isTransLoading ? (
+                    <>
+                      <Loader
+                        stroke="#D4E5FF"
+                        size="20px"
+                        style={{ marginRight: '10px' }}
+                      />
+                      Pending
+                    </>
+                  ) : (
+                    'Mint'
+                  )}
+                </SolidButton>
+              </Action>
+            </ActionContainer>
+          </Content>
+        </Wrapper>
+      </FullScreenTheme>
+
+      {isTransErrorModal && (
+        <ModalBackdrop setIsModalOpen={setIsTransErrorModal}>
+          <StatusModal setIsModalOpen={setIsTransErrorModal} isError />
+        </ModalBackdrop>
+      )}
+
+      {isTransSuccessModal && (
+        <ModalBackdrop setIsModalOpen={setIsTransSuccessModal}>
+          <StatusModal setIsModalOpen={setIsTransSuccessModal} />
+        </ModalBackdrop>
+      )}
+    </>
   );
 };
 
--- src/utils/blockchainUtils.ts
+++ src/utils/blockchainUtils.ts
@@ -1,16 +1,25 @@
 import { AbstractConnector } from '@web3-react/abstract-connector';
 import { connectors } from '../constants/connectors';
 import { networks } from '../constants/networks';
-import { getTokenContract, getTokenSaleContract, getTokenSaleContractAddress } from './contracts';
-import { buyToken } from './buyToken';
+import {
+  getTokenContract,
+  getTokenSaleContract,
+  getTokenSaleContractAddress,
+  getPlanetexTokenContractAddress,
+} from './contracts';
+import { buyToken, buyNftToken } from './buyToken';
 import { maxApproveAmount } from '../constants/constants';
 
-export const connectWallet = async (activate: any, connector: AbstractConnector, type: string) => {
+export const connectWallet = async (
+  activate: any,
+  connector: AbstractConnector,
+  type: string,
+) => {
   try {
-    await activate(connector)
+    await activate(connector);
     localStorage.setItem('provider', type);
   } catch (e) {
-    console.error(e)
+    console.error(e);
   }
 };
 
@@ -20,9 +29,9 @@ export const connectWalletOnPageLoad = (activate: any) => {
   if (provider) {
     try {
       // @ts-ignore
-      activate(connectors[provider])
+      activate(connectors[provider]);
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
   }
 };
@@ -32,90 +41,167 @@ export const disconnectWallet = (deactivate: any) => {
   deactivate();
 };
 
-export const changeNetwork = async(networkName: string) => {
+export const changeNetwork = async (networkName: string) => {
   try {
     // @ts-ignore
-    if (!window.ethereum) throw new Error('No crypto wallet found')
+    if (!window.ethereum) throw new Error('No crypto wallet found');
 
     if (networkName === 'goerli') {
       // @ts-ignore
       return await window.ethereum.request({
         method: 'wallet_switchEthereumChain',
-        params: [
-          {chainId: `0x${Number(5).toString(16)}`}
-        ]
-      })
+        params: [{ chainId: `0x${Number(5).toString(16)}` }],
+      });
     }
     // @ts-ignore
     await window.ethereum.request({
       method: 'wallet_addEthereumChain',
       params: [
         {
-          ...networks[networkName as keyof typeof networks]
-        }
-      ]
-    })
+          ...networks[networkName as keyof typeof networks],
+        },
+      ],
+    });
   } catch (e) {
-    console.error(e)
+    console.error(e);
   }
-}
+};
 
-export const checkAllowance = async (account: string | null | undefined, contract: any, spender: string) => {
-  return await contract.methods.allowance(account, spender).call()
-}
+export const checkAllowance = async (
+  account: string | null | undefined,
+  contract: any,
+  spender: string,
+) => {
+  return await contract.methods.allowance(account, spender).call();
+};
 
 export const checkApprove = async (
   chainId: number | undefined,
   account: string | null | undefined,
   tokenAmount: number | string,
-  tokenName:string,
+  tokenName: string,
   setIsTransSuccessModal: (b: boolean) => void,
   setIsTransErrorModal: (b: boolean) => void,
-  setIsTransLoading: (b: boolean) => void
+  setIsTransLoading: (b: boolean) => void,
 ) => {
   if (tokenName === 'USDT' || tokenName === 'BUSD') {
-    const tokenContract = getTokenContract(chainId)
-    const spender = getTokenSaleContractAddress(chainId)
+    const tokenContract = getTokenContract(chainId);
+    const spender = getTokenSaleContractAddress(chainId);
 
-    const allowance = await checkAllowance(account, tokenContract, spender)
+    const allowance = await checkAllowance(account, tokenContract, spender);
 
     if (allowance === '0') {
-      await approve(tokenContract, account, spender)
-        .then(() => buyToken(chainId, tokenAmount, account, tokenName, setIsTransSuccessModal, setIsTransErrorModal, setIsTransLoading))
+      await approve(tokenContract, account, spender).then(() =>
+        buyToken(
+          chainId,
+          tokenAmount,
+          account,
+          tokenName,
+          setIsTransSuccessModal,
+          setIsTransErrorModal,
+          setIsTransLoading,
+        ),
+      );
     }
 
-    return await buyToken(chainId, tokenAmount, account, tokenName, setIsTransSuccessModal, setIsTransErrorModal, setIsTransLoading)
+    return await buyToken(
+      chainId,
+      tokenAmount,
+      account,
+      tokenName,
+      setIsTransSuccessModal,
+      setIsTransErrorModal,
+      setIsTransLoading,
+    );
   } else {
-    return await buyToken(chainId, tokenAmount, account, tokenName, setIsTransSuccessModal, setIsTransErrorModal, setIsTransLoading)
+    return await buyToken(
+      chainId,
+      tokenAmount,
+      account,
+      tokenName,
+      setIsTransSuccessModal,
+      setIsTransErrorModal,
+      setIsTransLoading,
+    );
   }
-}
-
-export const approve = async (contract: any, account: string | null | undefined, spender: string) => {
-  return await contract.methods
-    .approve(spender, maxApproveAmount)
-    .send({ from: account })
-    // .on('transactionHash', function(hash) {})
-    // .on('receipt', function(receipt) {
-    //   setIsApprovePending(false)
-    // })
-    // .on('error', function(error, receipt) {
-    //   setIsApprovePending(false)
-    // })
-}
-
-export const convertToUSD = async (chainId: number | undefined, tokenAmount: number | string) => {
-  const contract = await getTokenSaleContract(chainId)
-  const formattedAmount = (+tokenAmount * 10 ** 18).toLocaleString('fullwide', { useGrouping: false })
-  const res = await contract.methods.convertToStable(formattedAmount, 0).call()
-  return res
 };
 
-export const getUserAvailableAmount = async (chainId: number | undefined, account: any) => {
-  const contract = await getTokenSaleContract(chainId)
-  return await contract.methods.getUserAvailableAmount(account, 0).call()
-}
+export const checkApproveNft = async (
+  chainId: number | undefined,
+  account: string | null | undefined,
+  tokenName: string,
+  setIsTransSuccessModal: (b: boolean) => void,
+  setIsTransErrorModal: (b: boolean) => void,
+  setIsTransLoading: (b: boolean) => void,
+) => {
+  if (tokenName === 'USDT') {
+    const tokenContract = getTokenContract(chainId);
+    const spender = getPlanetexTokenContractAddress(chainId);
+
+    const allowance = await checkAllowance(account, tokenContract, spender);
+
+    if (allowance === '0') {
+      await approve(tokenContract, account, spender).then(() =>
+        buyNftToken(
+          chainId,
+          account,
+          tokenName,
+          setIsTransSuccessModal,
+          setIsTransErrorModal,
+          setIsTransLoading,
+        ),
+      );
+    }
+  } else {
+    return await buyNftToken(
+      chainId,
+      account,
+      tokenName,
+      setIsTransSuccessModal,
+      setIsTransErrorModal,
+      setIsTransLoading,
+    );
+  }
+};
+
+export const approve = async (
+  contract: any,
+  account: string | null | undefined,
+  spender: string,
+) => {
+  return await contract.methods
+    .approve(spender, maxApproveAmount)
+    .send({ from: account });
+  // .on('transactionHash', function(hash) {})
+  // .on('receipt', function(receipt) {
+  //   setIsApprovePending(false)
+  // })
+  // .on('error', function(error, receipt) {
+  //   setIsApprovePending(false)
+  // })
+};
+
+export const convertToUSD = async (
+  chainId: number | undefined,
+  tokenAmount: number | string,
+) => {
+  const contract = await getTokenSaleContract(chainId);
+  const formattedAmount = (+tokenAmount * 10 ** 18).toLocaleString('fullwide', {
+    useGrouping: false,
+  });
+  const res = await contract.methods.convertToStable(formattedAmount, 0).call();
+  return res;
+};
+
+export const getUserAvailableAmount = async (
+  chainId: number | undefined,
+  account: any,
+) => {
+  const contract = await getTokenSaleContract(chainId);
+  return await contract.methods.getUserAvailableAmount(account, 0).call();
+};
 
 export const formatToHuman = (chainId: number | undefined, amount: string) => {
-  if (chainId === 56 || chainId === 97) return (+amount / 10 ** 18).toFixed(2)
-  return (+amount / 10 ** 6).toFixed(2)
+  if (chainId === 56 || chainId === 97) return (+amount / 10 ** 18).toFixed(2);
+  return (+amount / 10 ** 6).toFixed(2);
 };
