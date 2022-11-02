105f27f73497a920ab8e74cca80601a70db3e605
Serhii Stanislav <serhiistanislav@gmail.com>
2022-11-02 19:38:36 +0200

    ADD | lazy load

--- src/components/SaleSection/SaleSection.tsx
+++ src/components/SaleSection/SaleSection.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { SaleSectionProps } from '../../constants/types';
 import PreSaleContent from './PreSaleContent/PreSaleContent';
-import MintNFTSection from './MintNFTContent/MintNFTSection';
+
+const MintNFTSection = lazy(() => import('./MintNFTContent/MintNFTSection'));
 
 const SaleSection = ({ title }: SaleSectionProps) => {
   return (
     <div>
       {title === 'Pre-Sale' && <PreSaleContent />}
-      {title === 'NFT-Sale' && <MintNFTSection />}
+      {title === 'NFT-Sale' && (
+        <Suspense>
+          <MintNFTSection />
+        </Suspense>
+      )}
     </div>
   );
 };
