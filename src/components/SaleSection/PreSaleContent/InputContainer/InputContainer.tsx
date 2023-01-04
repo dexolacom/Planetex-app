88ce93fa06cca740e60458ceeed7cda783c74157
Serhii Stanislav <serhiistanislav@gmail.com>
2023-01-04 13:17:12 +0200

    EDIT | UserBalance

--- src/components/SaleSection/PreSaleContent/InputContainer/InputContainer.tsx
+++ src/components/SaleSection/PreSaleContent/InputContainer/InputContainer.tsx
@@ -1,7 +1,15 @@
+/* eslint-disable max-lines-per-function */
 import React, { useEffect } from 'react';
 import { useWeb3React } from '@web3-react/core';
 import { InputContainerProps } from '../../../../constants/types';
-import { Input, InputBlock, InputInfo, Select, Wrapper, SelectBlock } from './styles';
+import {
+  Input,
+  InputBlock,
+  InputInfo,
+  Select,
+  Wrapper,
+  SelectBlock,
+} from './styles';
 import ConvertedBalance from '../ConvertedBalance/ConvertedBalance';
 
 const InputContainer = ({
@@ -15,71 +23,87 @@ const InputContainer = ({
   setTokenAmount,
   setTokenName,
   setIsInputAmountError,
-  setIsApproveWarning
-}:InputContainerProps) => {
-
-  const { account, chainId } = useWeb3React()
+  setIsApproveWarning,
+}: InputContainerProps) => {
+  const { account, chainId } = useWeb3React();
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = (e.target.value).replace(/^0\d{1}$/, '').replace(/,/g, '.');
-    setIsApproveWarning(false)
+    const value = e.target.value.replace(/^0\d{1}$/, '').replace(/,/g, '.');
+    setIsApproveWarning(false);
     if (value.match(/^[0-9]*[.,]?[0-9]*$/)) {
       if (+value >= 10 || +value === 0) {
-        setIsInputAmountError(false)
+        setIsInputAmountError(false);
         return setTokenAmount(value);
       }
-      setIsInputAmountError(true)
+      setIsInputAmountError(true);
       return setTokenAmount(value);
     }
   };
 
   useEffect(() => {
     // get initial tokenName
-    if (chainId === 97 || chainId === 56 ) setTokenName('BNB')
-    if (chainId === 5 || chainId === 1) setTokenName('ETH')
+    if (chainId === 97 || chainId === 56) setTokenName('BNB');
+    if (chainId === 5 || chainId === 1) setTokenName('ETH');
   }, [chainId]);
 
   return (
-    <Wrapper marginBottom={(isInputAmountError || isWalletWarning || isApproveWarning) ? '5px' : '30px'}>
+    <Wrapper
+      marginBottom={
+        isInputAmountError || isWalletWarning || isApproveWarning
+          ? '5px'
+          : '30px'
+      }
+    >
       {/*@ts-ignore*/}
-      <InputBlock border={isInputAmountError ? '1px solid #582424' : '1px solid #372458'} filter={account ? '' : 'grayscale(1)'}>
+      <InputBlock
+        border={isInputAmountError ? '1px solid #582424' : '1px solid #372458'}
+        filter={account ? '' : 'grayscale(1)'}
+      >
         <Input
           disabled={!account}
-          type='text'
+          type="text"
           value={tokenAmount}
           onChange={(e) => handleInputChange(e)}
           placeholder={'0.0'}
-          inputMode='decimal'
+          inputMode="decimal"
           // @ts-ignore
-          maxLength='11'
+          maxLength="11"
         />
         {/*@ts-ignore*/}
-        <InputInfo color={!account ? '#762ACE' : '#5e626c'} border={!account ? '2px solid #762ACE' : '2px solid #5e626c'}>
+        <InputInfo
+          color={!account ? '#762ACE' : '#5e626c'}
+          border={!account ? '2px solid #762ACE' : '2px solid #5e626c'}
+        >
           {tokenName === 'USDT' || tokenName === 'BUSD'
             ? `${tokenAmount || '0.00'} $`
-            : `${convertedToUSD || '0.00'} $`
-          }
-
+            : `${convertedToUSD || '0.00'} $`}
         </InputInfo>
       </InputBlock>
 
       <SelectBlock>
-        <ConvertedBalance convertedToPltx={convertedToPltx}/>
+        <ConvertedBalance convertedToPltx={convertedToPltx} />
         {/*@ts-ignore*/}
-        <Select disabled={!account} filter={account ? '' : 'grayscale(1)'} color={!account ? '#5e626c' : ''} id='tokenSelect' value={tokenName} onChange={(e) => setTokenName(e.target.value)}>
-          {(chainId === 97 || chainId === 56 )
-            ? <>
-              <option value='BNB'>BNB</option>
-              <option value='BUSD'>BUSD</option>
+        <Select
+          disabled={!account}
+          filter={account ? '' : 'grayscale(1)'}
+          color={!account ? '#5e626c' : ''}
+          id="tokenSelect"
+          value={tokenName}
+          onChange={(e) => setTokenName(e.target.value)}
+        >
+          {chainId === 97 || chainId === 56 ? (
+            <>
+              <option value="BNB">BNB</option>
+              <option value="BUSD">BUSD</option>
             </>
-            : <>
-              <option value='ETH'>ETH</option>
-              <option value='USDT'>USDT</option>
+          ) : (
+            <>
+              <option value="ETH">ETH</option>
+              <option value="USDT">USDT</option>
             </>
-          }
+          )}
         </Select>
       </SelectBlock>
-
     </Wrapper>
   );
 };
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,9 @@
 import { getUserBalance } from './blockchainUtils';
 
-export const stringTrim = (string: string | null | undefined, maxLength: number) => {
+export const stringTrim = (
+  string: string | null | undefined,
+  maxLength: number,
+) => {
   if (!string) return string;
   if (maxLength < 1) return string;
   if (string.length <= maxLength) return string;
@@ -10,23 +13,36 @@ export const stringTrim = (string: string | null | undefined, maxLength: number)
   const toRemove = string.length - maxLength;
   const leftStrip = Math.ceil(toRemove / 2);
   const rightStrip = toRemove - leftStrip;
-  return `${string.substring(0, midpoint - leftStrip)}...${string.substring(midpoint + rightStrip)}`;
+  return `${string.substring(0, midpoint - leftStrip)}...${string.substring(
+    midpoint + rightStrip,
+  )}`;
 };
 
 export const getDecimals = (tokenName: string) => {
   const decimals = {
-    'BNB': 10 ** 18,
-    'BUSD': 10 ** 18,
-    'ETH': 10 ** 18,
-    'USDT': 10 ** 6
-  }
+    BNB: 10 ** 18,
+    BUSD: 10 ** 18,
+    ETH: 10 ** 18,
+    USDT: 10 ** 6,
+  };
 
-  return decimals[tokenName as keyof typeof decimals] ?? console.error('Token name is undefined')
+  return (
+    decimals[tokenName as keyof typeof decimals] ??
+    console.error('Token name is undefined')
+  );
 };
 
-export const getUserBalanceSum = async (account: any) => {
-  const busdBalance = await getUserBalance(56, account)
-  const usdtBalance = await getUserBalance(1, account)
-  const sum = (BigInt(usdtBalance) + BigInt(busdBalance)).toString()
-  return (+sum / 10 ** 18).toFixed()
+export const getUserBalanceSum = async (
+  // chainId: number | undefined,
+  account: any,
+) => {
+  // const busdBalance = await getUserBalance(chainId, account);
+  const busdBalance = await getUserBalance(56, account);
+
+  // const usdtBalance = await getUserBalance(chainId, account);
+  const usdtBalance = await getUserBalance(1, account);
+
+  return (Number(usdtBalance) + Number(busdBalance)).toString();
+  // const sum = (BigInt(usdtBalance) + BigInt(busdBalance)).toString();
+  // return (+sum / 10 ** 18).toFixed();
 };
